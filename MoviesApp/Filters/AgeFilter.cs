f01239b5897caee91856259f4e1cc18c76c20b9c
Sinde1 <filinkseniy@gmail.com>
2022-12-19 22:56:50 +0300

    Добавьте файлы проекта.

---
+++ MoviesApp/Filters/AgeFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+
+namespace MoviesApp.Filters {
+	public class AgeFilter : Attribute, IActionFilter {
+		public void OnActionExecuting(ActionExecutingContext context) {
+
+			DateTime filt = DateTime.Parse(context.HttpContext.Request.Form["Birthday"]);
+			if((DateTime.Now-filt).Year < 7 || (DateTime.Now-filt).Year > 99) {
+				context.Result = new BadRequestResult();
+			}
+		}
+		public void OnActionExecuted(ActionExecutedContext context) {
+
+		}
+	}
+
+}
---
+++ MoviesApp/ViewModels/InputActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.ViewModels {
+	public class InputActor {
+		[Required]
+		public string Name {get; set;}
+
+		[Required]
+		public string LastName {get; set;}
+
+		[Required]
+		public DateTime Birthday {get; set;}
+	}
+}
